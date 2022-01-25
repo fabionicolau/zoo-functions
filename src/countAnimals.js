346cf68e32df0ca6d34f93f7220a34bddce729c1
fabionicolau <fabionicolaujr@gmail.com>
2022-01-25 18:15:29 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const allAnimals = species.reduce((acc, objSpecies) => {
+      acc[objSpecies.name] = objSpecies.residents.length;
+      return acc;
+    }, {});
+    return allAnimals;
+  }
+
+  const findName = species.find((objSpecies) => objSpecies.name === animal.specie);
+  const filterSex = findName.residents.filter((objSpeciesSex) =>
+    objSpeciesSex.sex === animal.sex);
+
+  return animal.sex ? filterSex.length : findName.residents.length;
 }
 
 module.exports = countAnimals;

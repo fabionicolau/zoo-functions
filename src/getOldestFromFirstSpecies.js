a859e0f401e57219c4bb2dcb687488863c5486d8
fabionicolau <fabionicolaujr@gmail.com>
2022-01-25 19:26:11 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findEmployee = data.employees.find((objSpecies) => objSpecies.id === id);
+  const findSpecie = data.species.find((objSpecies) =>
+    objSpecies.id === findEmployee.responsibleFor[0]);
+  const findAnimals = findSpecie.residents.filter((objSpecies) => objSpecies.age);
+  const olderAnimal = findAnimals.sort((a, b) => b.age - a.age);
+  return Object.values(olderAnimal[0]);
 }
 
+console.log(getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = getOldestFromFirstSpecies;

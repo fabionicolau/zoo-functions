53979bf71de8aad9aad2a5941edd870fb58b6620
fabionicolau <fabionicolaujr@gmail.com>
2022-01-25 15:12:56 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((objSpecies) => ids.find((arrayIds) => objSpecies.id === arrayIds));
 }
 
 module.exports = getSpeciesByIds;

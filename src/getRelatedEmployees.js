bc1e7014f881dc3c44ede01386bab11d4c5c16bb
fabionicolau <fabionicolaujr@gmail.com>
2022-01-25 16:14:05 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((objEmployess) => objEmployess.managers.some((arrayID) => arrayID === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const findManagers = employees.filter((objEmployess) =>
+      objEmployess.managers.find((arrayID) => arrayID === managerId));
+    return findManagers.map((objEmployess) => `${objEmployess.firstName} ${objEmployess.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };

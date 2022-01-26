0930563fcbead0a36fd445c129271ef5ff500444
fabionicolau <fabionicolaujr@gmail.com>
2022-01-26 17:37:17 -0300

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const getEmployees = (nameOrId) => employees.find((objEmployees) =>
+  nameOrId.name === objEmployees.firstName || nameOrId.name === objEmployees.lastName
+  || nameOrId.id === objEmployees.id);
+
+const getSpecies = (ids) => species.filter((speciesID) =>
+  ids.find((arrayIds) => speciesID.id === arrayIds));
+
+const getEmployeesCoverageWithArgs = (nameOrId) => {
+  const objEmployees = getEmployees(nameOrId);
+  if (!objEmployees) {
+    throw new Error('Informações inválidas');
+  }
+  const specie = getSpecies(objEmployees.responsibleFor);
+  return {
+    id: objEmployees.id,
+    fullName: `${objEmployees.firstName} ${objEmployees.lastName}`,
+    species: specie.map((speciesName) => speciesName.name),
+    locations: specie.map((speciesLocation) => speciesLocation.location),
+  };
+};
+
+function getEmployeesCoverage(nameOrId) {
+  const allEmployees = employees.filter((objEmployees) => objEmployees);
+  if (!nameOrId) {
+    return allEmployees.reduce((acc, objEmployees) => {
+      const specie = getSpecies(objEmployees.responsibleFor);
+      const obj = {
+        id: objEmployees.id,
+        fullName: `${objEmployees.firstName} ${objEmployees.lastName}`,
+        species: specie.map((speciesName) => speciesName.name),
+        locations: specie.map((speciesLocation) => speciesLocation.location),
+      };
+      acc.push(obj);
+      return acc;
+    }, []);
+  }
+  const employeesCoverage = getEmployeesCoverageWithArgs(nameOrId);
+  return employeesCoverage;
 }
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -9,5 +9,4 @@ function getOldestFromFirstSpecies(id) {
   return Object.values(olderAnimal[0]);
 }
 
-console.log(getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = getOldestFromFirstSpecies;
